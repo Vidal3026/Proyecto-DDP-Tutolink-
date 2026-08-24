366e24f0c3f64eb84cb27523ac73c402c0fc1cd1
WILFREDO ANTONIO HERNÁNDEZ VIDAL <69657429+Vidal3026@users.noreply.github.com>
2026-08-24 09:49:00 -0600

    modificación de contraseña para variable del hash
    Updated comment to indicate password change.

--- generar_has.php
+++ generar_has.php
@@ -1,5 +1,5 @@
 <?php
-$contrasena_plana = "wil3026"; // 🔐 CÁMBIALA
+$contrasena_plana = "wil3026"; // cambiar contraseña
 $hash_seguro = password_hash($contrasena_plana, PASSWORD_BCRYPT);
 echo $hash_seguro;
 ?>
